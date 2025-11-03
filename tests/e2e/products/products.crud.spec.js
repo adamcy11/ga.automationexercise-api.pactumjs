87fa2b519e98a1a3bbc6c2802fd71434e98dc78e
Arthur Adamcy <arthuras1@hotmail.com>
2025-11-03 14:04:22 -0300

    adicionando alguns comments

--- tests/e2e/products/products.crud.spec.js
+++ tests/e2e/products/products.crud.spec.js
@@ -11,26 +11,33 @@ let createdProductPayload;
 describe('Produtos - CRUD completo', () => {
     
     before(async () => {
+        // Arrange
         const payload = { email: process.env.ADMIN_EMAIL, password: process.env.ADMIN_PASSWORD };
+        // Act
         const res = await spec().post('/login').withJson(payload).toss();
+        // Assert
         expect(res.statusCode).to.equal(200);
         authToken = res.body.authorization; 
     });
 
     it('GET /produtos — deve listar produtos (contrato OK)', async () => {
+        // Act
         const res = await spec().get('/produtos').toss();
+        // Assert
         expect(res.statusCode).to.equal(200);
         expectJoi(res.body, listProducts, 'listProducts');
     });
 
     it('POST /produtos — deve criar novo produto', async () => {
+        // Arrange
         createdProductPayload = makeProduct();
+        // Act
         const res = await spec()
             .post('/produtos')
             .withHeaders('Authorization', authToken)
             .withJson(createdProductPayload)
             .toss();
-
+        // Assert
         if (res.statusCode !== 201) {
             console.log(' POST /produtos falhou:', res.statusCode, res.body);
         }
@@ -42,33 +49,37 @@ describe('Produtos - CRUD completo', () => {
     });
 
     it('GET /produtos/:id — deve retornar o produto criado', async () => {
+        // Act
         const res = await spec().get(`/produtos/${productId}`).toss();
+        // Assert
         expect(res.statusCode).to.equal(200);
         expectJoi(res.body, productSchema, 'productById');
     });
 
     it('PUT /produtos/:id — deve atualizar o preço do produto', async () => {
+        // Arrange
         const updatePayload = {
             ...createdProductPayload, // mantém todos os campos exigidos pelo PUT
             preco: 199
         };
-
+       // Act
         const res = await spec()
             .put(`/produtos/${productId}`)
             .withHeaders('Authorization', authToken)
             .withJson(updatePayload)
             .toss();
-
+        // Assert
         expect(res.statusCode).to.equal(200);
        
     });
 
     it('DELETE /produtos/:id — deve deletar o produto criado', async () => {
+        // Act
         const res = await spec()
             .delete(`/produtos/${productId}`)
             .withHeaders('Authorization', authToken)
             .toss();
-
+        // Assert
         expect(res.statusCode).to.equal(200);
 
     });
